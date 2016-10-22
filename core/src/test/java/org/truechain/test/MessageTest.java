f89e0db42a3ab590ce1f4c41feae6095a5ca18af
ln <ln@DESKTOP-FPDAOR3>
2016-10-22 11:53:05 +0800

    .

--- core/src/test/java/org/truechain/test/MessageTest.java
+++ core/src/test/java/org/truechain/test/MessageTest.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
+ 
 public class MessageTest {
 
 //	public static void main(String[] args) {
